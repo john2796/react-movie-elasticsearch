0b4f7bfaa704889a2165c84e4c7bea041cb3df78
john2796 <benedictmiranda27@yahoo.com>
2018-10-31 15:01:59 -0700

    testing react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Switch, NavLink } from "react-router-dom";
+import "./App.css";
+
+import Home from "./components/home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <div>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+        </div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route component={Error} />
+        </Switch>
+      </>
     );
   }
 }
---
+++ src/components/Error.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Error = () => (
+  <div>
+    <h1>Error path does not exist</h1>
+  </div>
+);
+export default Error;
